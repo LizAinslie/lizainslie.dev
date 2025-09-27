09c52e81ce3e1e5093d379bfe44f1b26cda4c8ba
Liz Ainslie <lizainslie16@gmail.com>
2025-09-26 19:16:50 -0700

    feat: deploy to pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel/serverless";
 import mdx from "@astrojs/mdx";
 
 import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://lizainslie.dev',
+  site: "https://lizainslie.dev",
   integrations: [react(), tailwind(), mdx(), robotsTxt()],
-  output: "server",
-  adapter: vercel()
 });
