d594b88a4f40b1bdccebc911fc81956214f16bdc
Mey <mey@lizainslie.dev>
2025-12-10 09:41:31 -0700

    feat: tag listings, about page updates

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,6 +1,8 @@
 
 export default new Map([
-["src/data/blog/2025/11/19/site-updates.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fblog%2F2025%2F11%2F19%2Fsite-updates.mdx&astroContentModuleFlag=true")],
+["src/data/blog/2022/08/25/meta.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fblog%2F2022%2F08%2F25%2Fmeta.mdx&astroContentModuleFlag=true")],
+["src/data/blog/2023/12/31/year-in-review.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fblog%2F2023%2F12%2F31%2Fyear-in-review.mdx&astroContentModuleFlag=true")],
 ["src/data/blog/2025/11/18/2025-update.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fblog%2F2025%2F11%2F18%2F2025-update.mdx&astroContentModuleFlag=true")],
-["src/data/blog/2023/12/31/year-in-review.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fblog%2F2023%2F12%2F31%2Fyear-in-review.mdx&astroContentModuleFlag=true")]]);
+["src/data/blog/2022/10/03/rust.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fblog%2F2022%2F10%2F03%2Frust.mdx&astroContentModuleFlag=true")],
+["src/data/blog/2025/11/19/site-updates.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fblog%2F2025%2F11%2F19%2Fsite-updates.mdx&astroContentModuleFlag=true")]]);
 		
